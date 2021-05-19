b4dd7cca6db81c4a51286c67e9a4c91b7944f6dd
MichalPodlaszuk <75248302+MichalPodlaszuk@users.noreply.github.com>
2021-05-19 13:59:19 +0200

    random

---
+++ chatbot/nltk_utils.py
@@ -0,0 +1,18 @@
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+
+nltk.download('punkt')
+stemmer = PorterStemmer()
+
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+    return stemmer.stem(word)
+
+
+def bag_of_words(tokenized_sentence, all_words):
+    pass
