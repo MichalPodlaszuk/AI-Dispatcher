e0b7ca7ef86cfef059440190c95f2e012b2264fe
MichalPodlaszuk <75248302+MichalPodlaszuk@users.noreply.github.com>
2021-05-27 01:36:26 +0200

    improvement on the chatbot

--- chatbot/chat.py
+++ chatbot/chat.py
@@ -2,8 +2,8 @@ import random
 import json
 import torch
 from model import NeuralNet
-from nltk_utils import tokenize, bag_of_words, remove_stop
-from speech.speech_handling import speech_recognizer
+from nltk_utils import bag_of_words, remove_stop
+from speech.speech_handling import speech_recognizer, speak
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -12,7 +12,7 @@ with open('../data/data_clean/intents.json', 'r') as f, open('../data/data_clean
     levels = json.load(g)
 
 FILE = '../data/model_data/data.pth'
-FILE_LEVELS = '..data/model_data/data_levels.pth'
+FILE_LEVELS = '../data/model_data/data_levels.pth'
 data = torch.load(FILE)
 data_levels = torch.load(FILE_LEVELS)
 
@@ -40,14 +40,14 @@ model_levels.eval()
 
 bot_name = 'AI-Dispatcher'
 print(f'{bot_name}: Hello, this is 911 dispatcher, what is your emergency *this is a test version, say quit to stop*')
-last_response = ['Hello, this is 911 dispatcher, what is your emergency']
-while True:
-    sentence = speech_recognizer(chat=True, previous=last_response[-1])
-    if sentence == 'quit':
-        break
+last_response = ['Hello this is 911 dispatcher, what is your emergency']
+previous_tag = []
+location = []
+emergency = []
 
-    tokenized_sentence = tokenize(sentence)
-    sentence_without_stop_words = remove_stop(tokenized_sentence)
+
+def classify(sentence, all_words, model, tags, file):
+    sentence_without_stop_words = remove_stop(sentence)
     X = bag_of_words(sentence_without_stop_words, all_words)
     X_reshaped = X.reshape(1, X.shape[0])
     X_tensor = torch.from_numpy(X_reshaped).to(device)
@@ -60,22 +60,43 @@ while True:
     prob = probs[0][predicted.item()]
 
     if prob.item() > 0.6:
-        for intent in intents['intents']:
+        for intent in file['intents']:
             if tag == intent['tag']:
                 response = random.choice(intent["responses"])
-                print(f'{bot_name}: {response}, detected intent: {tag}')
-                last_response.append(response)
-                if tag == 'emergency':
-                    output_level = model_levels(X_tensor)
-                    _, predicted_levels = torch.max(output_level, dim=1)
-                    tag_levels = tags_levels[predicted_levels.item()]
-
-                    probs_levels = torch.softmax(output_level, dim=1)
-                    prob_levels = probs_levels[0][predicted_levels.item()]
-                    if prob.item() > 0.6:
-                        for level in levels['intents']:
-                            if tag == levels['tag']:
-                                action = random.choice(levels['responses'])
-                                print(f'action: {action}, detected level: {tag}')
+                if file == intents:
+                    print(f'{bot_name}: {response}, detected intent: {tag}')
+                else:
+                    print(f'{response}, detected tag: {tag}')
     else:
         print(f"{bot_name}: Please repeat your message more clearly")
+        response = 'Please repeat your message more clearly'
+        tag = 'unkn'
+    return tag, response
+
+
+while True:
+    sentence = speech_recognizer(chat=True, previous=last_response[-1])
+    if sentence == 'quit':
+        break
+
+    tag, response = classify(sentence, all_words, model, tags, intents)
+    last_response.append(response)
+    previous_tag = tag
+    if tag == 'emergency':
+        print(f'detected tag:{tag}, starting emergency classification')
+        level, action = classify(sentence, all_words_levels, model_levels, tags_levels, levels)
+        print(f'emergency level detected: {level}')
+        emergency.extend((sentence, level, action))
+        if level == '1' or level == '2':
+            print('redirecting')
+            speak('your call will be redirected please stay on the line')
+            break
+        else:
+            print('asking for location')
+            last_response.append('Okay, tell me where you are')
+    if tag == 'location' and emergency[1] == '3' or emergency[1] == '4':
+        location.append(sentence)
+        last_response.append('Okay tell me again exactly what happened')
+    if tag == 'random' and len(location) == 0:
+        last_response.append(random.choice(['I need to know your location please', 'try to find out where exactly you are']))
+        
